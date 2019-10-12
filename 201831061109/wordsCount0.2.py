55051ffd0a3af4bedf3a746e265f7fcc03cc81bd
F0urty-Tw0 <1678931512@qq.com>
2019-10-12 11:41:41 +0800

    version 1.1

--- 201831061109/wordsCount0.2.py
+++ 201831061109/wordsCount0.2.py
@@ -2,15 +2,17 @@
 import re
 import sys
 import getopt
-from sys import argv
 
+from sys import argv
 script, filename = argv
 
+global number = 0
 
 class word_operate():
-    def __init__(self, txt_file, contents):
+    def __init__(self, txt_file, contents, c_filename):
         self.txt_file = txt_file
         self.contents = contents
+        self.c_filename = c_filename
 
     def charsCount(self):
         chars = contents.rstrip()
@@ -38,13 +40,44 @@ class word_operate():
         counts_list = list(count_num.items())  # 将字典转化为列表，方便后续输出
         counts_list.sort(key=lambda x: x[1], reverse=True)  # 对列表中每个元素的第二个字段排序，及单词出现频率
 
-        for i in range(10):
+        for i in range(number):
             word, count = counts_list[i]
             print(f'{word}:{count}')
 
-txt_file = open(filename,'rb')
-contents = txt_file.read()
-wordCount = word_operate(txt_file, contents)
-wordCount.charsCount()
-wordCount.linesCount()
-wordCount.wordsCount()
+
+def findword(n):#传入指定长度参数n
+    words_arr = re.findall(r'\w{4,}', contents.lower())  # 匹配字符数大于4的单词
+    wordDict = {}
+    for word in words_arr:
+        wordDict[word] = wordDict.get(word, 0) + 1  # 利用字典的get方法，将单词作为键，方便查询避免单词重复出现
+    for key in wordDict
+        if len(key) == n:     #计算键长与目标长度匹配
+            print (f'{key}\n')
+
+
+def main(argv):
+    #filename = ''
+    try:
+        opts, args = getopt.getopt(argv, "i:mn", ["path=", "phrase=","numbers"])
+    except getopt.GetoptError:
+        print 'test.py -i <filepath> -m <number_of_phrase> -n <number_of_words>'
+    for opt,arg in opts:
+        if opt == '-i':
+            filename = arg
+        elif opt == '-n':
+            number = arg
+
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+    if filename == '':
+        print("Please enter the '-i' parameter!")
+        sys.exit(0)
+    else:
+        txt_file = open(filename,'rb')
+        contents = txt_file.read()
+        wordCount = word_operate(txt_file, contents)
+        wordCount.charsCount()
+        wordCount.linesCount()
+        wordCount.wordsCount()
